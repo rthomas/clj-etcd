168a00b8315fff90fbbbea2ca49400cd59e5f3af
Ryan Thomas <rthomas@atlassian.com>
2013-11-26 07:23:54 +1100

    lein-release plugin: bumped version from 0.1.2 to 0.1.3-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-etcd "0.1.2"
+(defproject clj-etcd "0.1.3-SNAPSHOT"
   :description "etcd client library for Clojure"
   :url "http://github.com/rthomas/clj-etcd"
   :license {:name "Eclipse Public License"
